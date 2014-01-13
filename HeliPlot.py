855e21f33e783f6c249b38d7742e05d8bcc39d2b
Alejandro Gonzales <agonzales@usgs.gov>
2014-01-13 10:49:23 -0700

    Update HeliPlot.py
    Set datetimeQuery variable to system time.

--- HeliPlot.py
+++ HeliPlot.py
@@ -619,8 +619,8 @@ class HeliPlot(object):
 		timestring = re.split("\\.", timestring)
 		tmp = timestring[0]
 		timedate = tmp.replace("-", "/")
-		#datetimeQuery = timedate.strip()
-		datetimeQuery = "2013/09/12 13:30:00"
+		datetimeQuery = timedate.strip()
+		#datetimeQuery = "2013/09/12 13:30:00"
 		self.datetimeQuery = datetimeQuery
 		tmpquery = re.split(' ', self.datetimeQuery)
 		tmpdate = tmp[0].strip()
